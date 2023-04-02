e78712eaa8f3af9f974cfb944607460f333c5388
dale <hessdale96@gmail.com>
2023-04-01 19:25:54 -0600

    now cart displays on cart page

--- js/checkout.js
+++ js/checkout.js
@@ -1,13 +1,13 @@
 let selected_items = Cookies.get(`cart`);
 let product = document.getElementById(`product`);
-let body_html = document.querySelector(`body`);
-let cart_parse = JSON.parse(selected_items);
 
+let cart_parse = JSON.parse(selected_items);
+shopping_cart.push(cart_parse)
 if (Cookies.get(`cart`) === undefined) {
-  body_html.insertAdjacentHTML(`afterend`, `<h1 class="grid_align_center" class="text_align_center">EMPTY CART</h1>`)
+  product.insertAdjacentHTML(`afterend`, `<h1 class="grid_align_center" class="text_align_center">EMPTY CART</h1>`)
 } else {
-  for (i = 0; i < items.length; i++) {
-    body_html.insertAdjacentHTML(`afterend`, `<article id="product">
+  for (i = 0; i < shopping_cart[0].length; i++) {
+    product.insertAdjacentHTML(`afterend`, `<article id="product">
   <div id="product">
     <img src="${cart_parse[i].img}" alt="camera product">
   </div>
@@ -18,6 +18,7 @@ if (Cookies.get(`cart`) === undefined) {
   </article>`)
   }
 };
+
 
 
 
